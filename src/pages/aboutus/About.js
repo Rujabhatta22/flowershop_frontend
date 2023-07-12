9563625f481d166e685e1d98e01c5e67cdbe8709
rujabhatta22 <rujabhatta22@gmail.com>
2023-07-13 01:40:26 +0545

    about us

--- src/pages/aboutus/About.js
+++ src/pages/aboutus/About.js
@@ -1,24 +1,24 @@
-import "./about.css";
+import React from 'react';
+import './about.css';
 
 export default function About() {
   return (
-    <div className="sidebarItem">
-              <span className="sidebarTitle">ABOUT US</span>
-              <img src="images/about.png" alt="aboutus" />
-              <p>
-                  Welcome to Inkspire!
-                  We are passionate about creating a platform that empowers individuals to share their thoughts, experiences, and stories with the world.
-        Our goal is to provide a space where people from all walks of life can come together, connect, and engage in meaningful conversations.
-        Our team is comprised of dedicated individuals who are enthusiastic about technology, writing, and the power of community. We strive to create a user-friendly and intuitive platform that allows you to focus on what matters most – your content. We're constantly working behind the scenes to improve our features and ensure a seamless experience for both writers and readers.
-              </p>
-      <h1>
-        Our Mission and Vision
-      </h1>
-      <p>
-        At Inkspire, our mission is to provide a platform for passionate individuals to share their knowledge, experiences, and insights with a global community. Our mission is to inspire, educate, and entertain our readers by offering a wide range of high-quality, thought-provoking content across various topics and interests.
-        Our vision is to become the go-to destination for both readers and writers, creating a vibrant community where ideas are shared, conversations are sparked, and meaningful connections are formed.
-        Our vision is to create a seamless and intuitive blogging platform that offers robust features, easy navigation, and a visually appealing design, ensuring that both writers and readers can fully immerse themselves in the content.
-      </p>
-          </div>
+    <div className="aboutContainer">
+      <div className="aboutContent">
+        <h2 className="aboutTitle">ABOUT US</h2>
+        <img src="https://www.searchenginejournal.com/wp-content/uploads/2022/01/about-us-page-examples-1-61fd8f9784626-sej-384x202.jpg" alt="aboutus" className="aboutImage" />
+        <div className="aboutText">
+          <p className="aboutDescription">
+            Inkspire is dedicated to empowering individuals to share their thoughts, experiences, and stories with the world. Our platform brings people together, fostering meaningful conversations across various topics.
+            Our team comprises passionate individuals who believe in the power of community, technology, and writing. We strive to create an intuitive and user-friendly platform, allowing you to focus on your content. We continuously work behind the scenes to enhance features and provide a seamless experience for writers and readers.
+          </p>
+          <h3 className="aboutMissionVisionTitle">Our Mission and Vision</h3>
+          <p className="aboutMissionVisionDescription">
+            Our mission is to provide a platform for passionate individuals to share their knowledge, experiences, and insights globally. We aim to inspire, educate, and entertain our readers by offering a wide range of high-quality, thought-provoking content across various interests.
+            Our vision is to become the go-to destination for readers and writers, creating a vibrant community where ideas are shared, conversations are sparked, and meaningful connections are formed. We strive to create a visually appealing, seamless, and intuitive blogging platform that allows writers and readers to fully immerse themselves in the content.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
